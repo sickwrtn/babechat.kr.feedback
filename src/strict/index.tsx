37dd61e7d139ec56d3b5ea4057c41150f70d8ffa
sickwrtn <sillo154265@gmail.com>
2025-05-25 02:00:12 +0900

    asf

--- src/strict/index.tsx
+++ src/strict/index.tsx
@@ -1,10 +1,17 @@
-console.log(" ____  ____  ____  ____  ____  ____ \n||S ||||T ||||R ||||I ||||C ||||T ||\n||__||||__||||__||||__||||__||||__||\n|/__\\||/__\\||/__\\||/__\\||/__\\||/__\\|\n           [STRICT MODULE]");
+console.log(" ____  ____  ____  ____  ____  ____ \n||S ||||T ||||R ||||I ||||C ||||T ||\n||__||||__||||__||||__||||__||||__||\n|/__\\||/__\\||/__\\||/__\\||/__\\||/__\\|\n           [STRICT MODE]\n           Made By Sillo");
+console.log("TThe Strict module supports very strict function definitions. \nAll functions, including anonymous functions, must be exception-handled by default, \nand all types must be explicit.")
+
 /*
  ____  ____  ____  ____  ____  ____ 
 ||S ||||T ||||R ||||I ||||C ||||T ||
 ||__||||__||||__||||__||||__||||__||
 |/__\||/__\||/__\||/__\||/__\||/__\|
-           [STRICT MODULE]
+           [STRICT MODE]
+           Made By Sillo
+           
+TThe Strict module supports very strict function definitions. 
+All functions, including anonymous functions, must be exception-handled by default, 
+and all types must be explicit.
 */
 
 
