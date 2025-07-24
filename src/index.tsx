fff4b4d79956a2eeb065fd1f812226df4c0ac371
sickwrtn <sillo154265@gmail.com>
2025-07-25 05:16:41 +0900

    LSMP SCOP

--- src/index.tsx
+++ src/index.tsx
@@ -21,7 +21,7 @@ import { Taeho } from './component';
 function Index() {
 
     //i18n 선언
-    const { t,i18n } = useTranslation();
+    const { t } = useTranslation();
 
     setStrict(()=>{})
     // auth_token 이 localStorage에 없을시 발급
